10aeb817da2ea76619dc0fb016890990799f235a
Micah Alpern <malpern@gmail.com>
2025-03-15 20:43:09 -0700

    Initial Commit

---
+++ cursor-window/cursor_windowApp.swift
@@ -0,0 +1,17 @@
+//
+//  cursor_windowApp.swift
+//  cursor-window
+//
+//  Created by Micah Alpern on 3/15/25.
+//
+
+import SwiftUI
+
+@main
+struct cursor_windowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
